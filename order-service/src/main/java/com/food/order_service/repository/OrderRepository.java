31d996b8b52e69ca12b3c12ef8666363c41c276e
Sridhar S <sridhar15062001@gmail.com>
2026-06-03 12:52:06 +0530

    Made some query optimization in order-service

--- order-service/src/main/java/com/food/order_service/repository/OrderRepository.java
+++ order-service/src/main/java/com/food/order_service/repository/OrderRepository.java
@@ -1,16 +1,23 @@
 package com.food.order_service.repository;
 
 import com.food.order_service.entity.OrderEntity;
+
+import org.springframework.data.repository.query.Param;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
-    List<OrderEntity> findByCustomerId(Long customerId);
 
-    List<OrderEntity> findByRestaurantId(Long restaurantId);
+    @Query("SELECT o FROM OrderEntity o JOIN FETCH o.items WHERE o.customerId = :customerId")
+    List<OrderEntity> findByCustomerId(@Param("customerId") Long customerId);
+
+    @Query("SELECT o FROM OrderEntity o JOIN FETCH o.items WHERE o.restaurantId = :restaurantId")
+    List<OrderEntity> findByRestaurantId(@Param("restaurantId") Long restaurantId);
 
     void deleteByCustomerId(Long customerId);
 }
